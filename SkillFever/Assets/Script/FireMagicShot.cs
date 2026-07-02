4c697b43febb9446a9c5341861e6363c04fc6a27
yochi513 <yojrdkt@gmail.com>
2026-07-02 15:47:05 +0900

    杖の追加。魔法を撃った時に杖に反動の追加

--- SkillFever/Assets/Script/FireMagicShot.cs
+++ SkillFever/Assets/Script/FireMagicShot.cs
@@ -3,14 +3,27 @@ using UnityEngine.InputSystem;
 
 public class FireMagicShot : MonoBehaviour
 {
+    //ñÇñ@
    public GameObject fireBallPrefab;
    public Transform shotPoint;
+
+    //èÒ
+    [SerializeField] private Transform wand;
+    private Quaternion originalRotation;
+     void Start()
+    {
+        originalRotation = wand.localRotation;
+
+    }
+
 
     void Update()
     {
         if (Keyboard.current.eKey.wasPressedThisFrame)
         {
             Instantiate(fireBallPrefab, shotPoint.position, shotPoint.rotation);
+            wand.localRotation *= Quaternion.Euler(-10f,0f,0f);
         }
+        wand.localRotation = Quaternion.Slerp(wand.localRotation,originalRotation, Time.deltaTime * 10f);
     }
 }
